56815653d4d2a70ee856fd89ccebb4de8b9cbb12
kay <ickang@enliple.com>
2020-10-26 14:11:37 +0900

    Add framework 'DGActivityIndicator'

---
+++ DGActivityIndicator/DGActivityIndicator.h
@@ -0,0 +1,19 @@
+//
+//  DGActivityIndicator.h
+//  DGActivityIndicator
+//
+//  Created by kay on 2020/10/26.
+//  Copyright © 2020 Danil Gontovnik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DGActivityIndicator.
+FOUNDATION_EXPORT double DGActivityIndicatorVersionNumber;
+
+//! Project version string for DGActivityIndicator.
+FOUNDATION_EXPORT const unsigned char DGActivityIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DGActivityIndicator/PublicHeader.h>
+
+
